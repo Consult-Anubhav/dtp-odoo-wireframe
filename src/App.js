2dae58046b160b64f408e318f2a546cce136a1e0
Sachin Bhoi <givesachin@gmail.com>
2023-01-10 13:11:40 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header style={{height: '240px'}}>
+      </header>
+      <div style={{minHeight: 'calc(100vh - 480px)', backgroundColor: '#EEEEEE'}}>
+
+      </div>
+      <footer style={{height: '240px'}}>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
